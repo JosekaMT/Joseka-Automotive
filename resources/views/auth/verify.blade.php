265bc137b39a50ababfc578f75cd0ff0dc1b81b3
Joseka <josekamt@alumnos.ilernasevilla.com>
2024-05-16 15:15:13 +0200

    mas cambios

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -1,24 +1,33 @@
-@extends('layouts.app')
+@extends('layouts.auth')
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
+    <div class="row justify-content-center align-items-center" style="height: 100vh;">
+        <div class="col-md-6 col-lg-4">
 
+            <a href="{{ url('/') }}" class="return-icon">
+                <i class="fas fa-arrow-left"></i>
+            </a>
+            <div class="card" style="background-color: rgba(0, 0, 0, 0.8); color: white; padding: 20px; border-radius: 10px; text-align: center;">
                 <div class="card-body">
+                    <div class="d-flex justify-content-center mb-4">
+                        <a href="{{ url('/') }}">
+                            <img src="{{ asset('img/logo.png') }}" alt="Logo" style="width: 260px; height: 70px;">
+                        </a>
+                    </div>
+
                     @if (session('resent'))
                         <div class="alert alert-success" role="alert">
                             {{ __('A fresh verification link has been sent to your email address.') }}
                         </div>
                     @endif
 
-                    {{ __('Before proceeding, please check your email for a verification link.') }}
-                    {{ __('If you did not receive the email') }},
+                    <p>{{ __('Before proceeding, please check your email for a verification link.') }}</p>
+                    <p>{{ __('If you did not receive the email') }}</p>
+
                     <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
                         @csrf
-                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
+                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline" style="color: #9c2121;">{{ __('click here to request another') }}</button>.
                     </form>
                 </div>
             </div>
