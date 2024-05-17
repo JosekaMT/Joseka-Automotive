7bdf712fa3a5d80562264e2c72ca4cc3fc65d732
Joseka <josekamt@alumnos.ilernasevilla.com>
2024-05-17 20:50:18 +0200

    scroll

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -159,6 +159,7 @@
     <script src="../assets/js/core/popper.min.js"></script>
     <script src="../assets/js/core/bootstrap.min.js"></script>
     <script src="../assets/js/plugins/smooth-scrollbar.min.js"></script>
+    <script src="../assets/js/plugins/perfect-scrollbar.min.js"></script><!--quitar, scroll raro pero sino no va el aside -->
     <script src="../assets/js/plugins/chartjs.min.js"></script>
 
 
